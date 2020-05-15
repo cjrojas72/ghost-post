366d5c1b46418064d64fa9cfc1b686c77c580c50
Christian Rojas <cjrojas72@gmail.com>
2020-05-15 14:29:21 -0400

    init commit and setup

---
+++ .venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/chris/Desktop/Kenzie Academy/PythonQ4/ghost-post/.venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

67c6ca73ba59241a6fe08b5ed7f7ebcfcf624c52
Christian Rojas <cjrojas72@gmail.com>
2020-05-15 15:23:51 -0400

    index page setup

--- ghost_post/urls.py
+++ ghost_post/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from posts.urls import urlpatterns as posts_urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += posts_urls
